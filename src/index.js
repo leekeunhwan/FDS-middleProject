d0bfe9e4da7ef26862131a652e9ab06442cbb11a
kimdongwon <ehddnjs8989@gmail.com>
2018-05-23 12:05:18 +0900

    깔끔하게 하기위한 template 도입

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 axios.get('http://localhost:3000/posts').then(res=>{
+  const listFragment = document.importNode(document.querySelector("#post-list").content, true);
   res.data.forEach(post=>{
     const fragment = document.importNode(document.querySelector('#post-item').content, true);
     const pEl = fragment.createElement('.post-item__title');
     pEl.textContent = post.title;
-    document.querySelector('.post-list').appendChild(fragment);
+    listFragment.querySelector('.post-list').appendChild(fragment);
   })
+
+  document.querySelector('.root').appendChild(listFragment);
 })
